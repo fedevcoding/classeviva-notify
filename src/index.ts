dd9d02e29b438993d70e31a40998c5ad137c7968
Federico <federicocavallini100@gmail.com>
2023-11-06 14:13:10 +0100

    add readme & remove express server

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ require("module-alias/register");
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
+// import express from "express";
 import { connectWebsockets } from "./main";
 // import { useRoutes } from "./routes/useRoutes";
 import { startBot } from "./bot";
 
-const port = process.env.PORT || 3000;
-const app = express();
+// const port = process.env.PORT || 3000;
+// const app = express();
 
 // useRoutes(app);
 
-app.listen(port, async () => {
-  connectWebsockets();
-  startBot();
-  console.log(`App listening at http://localhost:${port}`);
-});
+// app.listen(port, async () => {
+connectWebsockets();
+startBot();
+// console.log(`App listening at http://localhost:${port}`);
+// });
